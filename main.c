6d3431e2e96faa07e422ee5182d0f2f4c35f8aa2
connor.jarvis <connor.jarvis@defendtex.com>
2021-06-09 22:02:32 +1000

    Added comment for later, input system

--- main.c
+++ main.c
@@ -210,6 +210,17 @@ int main(int argc, char **argv)
 
 
 
+// Saving this for improving the keyboard input
+// int main ( int argc, char *argv[], char *env[] )
+// {
+//     gtk_init(&argc, &argv);
+
+//     GdkModifierType button_state;
+//     gdk_window_get_pointer(NULL, NULL, NULL, &button_state);
+//     if(button_state & GDK_CONTROL_MASK) {
+//         printf("ctrl key is pressed");
+//     }
+// }
 
 
 
